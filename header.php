5f201c8bcd803566411628be9a08702ce821d790
pey849 <peteryeung79@gmail.com>
2014-01-22 13:13:21 -0600

    Revert a8b4ae1..78f4803
    This rolls back to commit a8b4ae157cf90ac2419317e023c4b16165aa995c.

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ foreach($pages as $url=>$page){
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>CSSS Website</title>
+    <title>CSSS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
